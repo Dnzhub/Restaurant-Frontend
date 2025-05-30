a3888a651145f54d203a72cff1ef37eec74a7e90
Dnzhub <dnz.ylmz1@outlook.com>
2025-05-30 16:48:35 +0300

    Setup webpack, create home page dynamically

---
+++ src/home.js
@@ -0,0 +1,34 @@
+const content = document.querySelector("#content");
+import logoImage from "./images/logo.png";
+
+export default function createHomePage() {
+    // const image = document.createElement("img");
+    // image.src = pepegaImage;
+
+    // document.body.appendChild(image);
+
+    content.classList.add("home-tab");
+    const logo = document.createElement("img");
+    logo.src = logoImage;
+    logo.classList.add("logo");
+
+    const title = document.createElement("h1");
+    title.textContent = "Gold Shire"
+    title.style.fontSize = "3rem";
+    title.style.color = "#f1ddb0"
+
+    const text1 = document.createElement("p");
+    text1.textContent = "Warm hearths, frothy mugs, and whispered tales where every night sings with merriment and mystery";
+    text1.style.fontSize = "1.9rem";
+    text1.style.color = "#f1ddb0"
+
+    const text2 = document.createElement("p");
+    text2.textContent = "Enter, rest thy boots, and let the mead flow!"
+    text2.style.fontSize = "1.6rem"
+    text2.style.color = "#f1ddb0"
+
+    content.appendChild(title);
+    content.appendChild(logo);
+    content.appendChild(text1);
+    content.appendChild(text2);
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import { greeting } from "./greeting.js";
+import createHomePage from "./home.js";
+// import pepegaImage from "./images/pepega.jpeg"
+
+// console.log(greeting);
+// const image = document.createElement("img");
+// image.src = pepegaImage;
+
+// document.body.appendChild(image);
+
+
+createHomePage();
